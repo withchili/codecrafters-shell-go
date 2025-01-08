7d5305045189dea478011e5f423f040124e2e770
withchili <sergei696c696e@gmail.com>
2025-01-08 22:08:41 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+var builtinCommands = map[string]bool{
+	"type": true,
+	"exit": true,
+	"echo": true,
+}
+
 func exit(arguments []string) {
 	if arguments[0] == "0" {
 		os.Exit(0)
@@ -20,7 +26,18 @@ func echo(arguments []string) {
 	fmt.Print("\n")
 }
 
+func typeCommand(arguments []string) {
+	arg := arguments[0]
+	if _, ok := builtinCommands[arg]; ok {
+		fmt.Println(arg, "is a shell builtin")
+	} else {
+		fmt.Println(arg + ": not found")
+	}
+}
+
 func main() {
+	
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
@@ -43,6 +60,8 @@ func main() {
 			exit(arguments)
 		case "echo":
 			echo(arguments)
+		case "type":
+			typeCommand(arguments)
 		default:
 			fmt.Println(command + ": command not found")
 		}
